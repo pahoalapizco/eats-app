dd283fdf61270754c21e29af277347df3544216d
Paola Alapizco <pahoc.alapizco@gmail.com>
2019-09-12 16:59:55 -0600

    Query al carrito en BD

--- src/components/carrito/index.js
+++ src/components/carrito/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 import { withRouter } from "react-router";
 import CarritoVacio from '../utils/carritoVacio';
 import Carrito from './carrito';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import Loading from '../utils/loading';
+
+const GET_CARRITO =  gql`
+{
+  getCarrito {
+    _id
+    total
+    restaurant{
+      _id
+      name
+      open
+      closed
+      address
+    }
+    detail {
+      platillo {
+        _id
+      }
+      cantidad
+      importe
+    }
+  }
+}
+`;
 
 /**
- * @TODO: Query para obtener carrito guardado (buzón)
  * @TODO: Si hay carrito agregar nuevos elementos agregados 
  * @TODO: Hacer la mutation addCarrito.
  * @TODO: Finalizar pedido.
@@ -12,7 +37,44 @@ import Carrito from './carrito';
  * @TODO: Sigen pendientes las rutas publicas y privadas >..<
  */
 const CarritoContainer = ({ history }) => {
-  const carritoRender = history.location.state ?  <Carrito /> : <CarritoVacio />
+  const { loading, error, data } = useQuery(GET_CARRITO);
+  let carrito;
+
+  if (loading) return (<Loading />);
+
+  if (data.getCarrito && history.location.state) {
+    carrito = {
+      restaurant: data.getCarrito.restaurant._id,
+      total: data.getCarrito.total,
+      detail: []
+    };
+    // Agrega el detallado de BD al nuevo carrito
+    data.getCarrito.detail.forEach(d => {
+      carrito.detail.push({
+        platillo: d.platillo._id,
+        cantidad: d.cantidad,
+        importe: d.importe
+      });
+    });    
+
+    // Agrega detallado seleccionado
+    history.location.state.carrito.detail.forEach(d => {
+      carrito.detail.push({
+        platillo: d.platillo,
+        cantidad: d.cantidad,
+        importe: d.importe
+      });
+    });    
+    // calcula el nuevo total.
+    carrito.total +=  history.location.state.carrito.total
+  }  else {
+    carrito = history.location.state ? history.location.state.carrito : data.getCarrito;
+  }
+
+  console.log('carrito: ',  carrito);
+  console.log('state-carrito', history.location.state.carrito);
+
+  const carritoRender = carrito ?  <Carrito /> : <CarritoVacio />
   return(
     <div>
       {carritoRender}
